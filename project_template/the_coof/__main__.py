7741ec4dc6ce0685427bb4641da60c24bcc4d751
NickCampbell91 <nickcampbell1991@gmail.com>
2021-06-23 09:41:54 -0600

    Added a few files

--- project_template/the_coof/__main__.py
+++ project_template/the_coof/__main__.py
@@ -6,3 +6,13 @@ from data.gravity import Gravity
 from data.obstacles import Obstacles
 from data.point import Point
 from data.score import Score
+
+def main():
+    """ Main method """
+    window = MyGame()
+    window.setup()
+    arcade.run()
+
+
+if __name__ == "__main__":
+    main()
