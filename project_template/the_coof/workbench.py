5a958722049fbe780275eefb23a0f704af2bf8d8
NickCampbell91 <nickcampbell1991@gmail.com>
2021-06-24 15:38:14 -0600

    Cut some fat

--- project_template/the_coof/workbench.py
+++ project_template/the_coof/workbench.py
@@ -189,18 +189,6 @@ class MyGame(arcade.Window):
             if self.physics_engine.can_jump():
                 self.player_sprite.change_y = PLAYER_JUMP_SPEED
                 arcade.play_sound(self.jump_sound)
-        # elif key == arcade.key.LEFT or key == arcade.key.A:
-        #     self.player_sprite.change_x = -PLAYER_MOVEMENT_SPEED
-        # elif key == arcade.key.RIGHT or key == arcade.key.D:
-        #     self.player_sprite.change_x = PLAYER_MOVEMENT_SPEED
-
-    def on_key_release(self, key, modifiers):
-        """Called when the user releases a key. """
-
-        if key == arcade.key.LEFT or key == arcade.key.A:
-            self.player_sprite.change_x = PLAYER_MOVEMENT_SPEED
-        elif key == arcade.key.RIGHT or key == arcade.key.D:
-            self.player_sprite.change_x = PLAYER_MOVEMENT_SPEED
 
     def update(self, delta_time):
         """ Movement and game logic """
